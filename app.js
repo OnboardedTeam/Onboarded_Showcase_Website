2fbe3f61c6b60ad447d5d2de2a1f0ba966447322
Martin <61696199+NitramO-YT@users.noreply.github.com>
2023-06-20 19:33:38 +0200

    Adding modal iframe - 20/06/2023 19h30

--- app.js
+++ app.js
@@ -7,3 +7,57 @@ document.addEventListener('DOMContentLoaded', function() {
         h1.classList.add('show');
     }, 1000);
 });
+
+
+// Modal show/hide
+
+function openModal(href) {
+    var modalContainer = document.querySelector(".modalContainer");
+    var modalIframe = modalContainer.querySelector("iframe");
+
+    modalIframe.src = href;
+    modalContainer.removeAttribute("hidden");
+
+    modalIframe.addEventListener("load", (eventMainIframe) => {
+        modalIframe.addEventListener("load", (eventIframe) => {
+            eventIframe.preventDefault(); // prevent navigation
+            closeModal();
+        }, { once: true });
+    }, { once: true });
+}
+
+function closeModal() {
+    var modalContainer = document.querySelector(".modalContainer");
+
+    if (!modalContainer.hasAttribute("hidding")) {
+        modalContainer.setAttribute("hidding", "");
+
+        modalContainer.addEventListener("animationend", (eAnim) => {
+            if (eAnim.animationName === "fadeOut") {
+                modalContainer.removeAttribute("hidding");
+                modalContainer.setAttribute("hidden", "");
+            }
+        }, { once: true });
+    }
+}
+
+window.addEventListener("click", (event) => {
+    var target = event.target;
+
+    var modalContainer = document.querySelector(".modalContainer");
+    var modalIframe = modalContainer.querySelector("iframe");
+
+    if (target.tagName === "A" && target.getAttribute("href")) {
+        var href = target.getAttribute("href");
+
+        if (target.hasAttribute("openModal") && target.getAttribute("openModal") !== "false") {
+            event.preventDefault(); // cancel navigation
+
+            openModal(href);
+        }
+    } else if (!modalContainer.hasAttribute("hidden") && !modalContainer.hasAttribute("hidding")) {
+        if ((target === modalContainer && target !== modalIframe) || (target.tagName === "BUTTON" && target.classList.contains("goBack"))) {
+            closeModal();
+        }
+    }
+});
